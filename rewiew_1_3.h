7f457efcfba3a3f85bba892da194b8cc6ddfb967
IvshinKonstantin <ivshin1997@gmail.com>
2026-03-18 02:01:01 +0500

    Add files via upload

---
+++ rewiew_1_3.h
@@ -0,0 +1,13 @@
+#ifndef REWIEW_1_3_H
+#define REWIEW_1_3_H
+
+// Функция для безопасного возведения в степень (целые числа)
+long long SafePower(int base, int exponent);
+
+// Функция для подсчета чисел с 4 или более подряд идущими нулями
+double CountNumbersWithFourPlusZeros(int num_digits, int base);
+
+// Основная функция решения задачи
+void SolveTask();
+
+#endif // REWIEW_1_3_H
