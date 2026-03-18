9ea25e1ec4ed276ec7f8794beb943e38382b5d9a
IvshinKonstantin <ivshin1997@gmail.com>
2026-03-18 10:42:12 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 #include "rewiew_1_3.h"
 
 int main() {
-    setlocale(LC_ALL, "russian");
-    SolveTask();
-    return 0;
+  setlocale(LC_ALL, "russian");
+  SolveTask();
+  return 0;
 }
