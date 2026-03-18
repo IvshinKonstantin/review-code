6ed3c8046dcd2440c2b1641d12a7160abde346fb
IvshinKonstantin <ivshin1997@gmail.com>
2026-03-18 10:48:18 +0500

    Create rewiew_1_1.h

---
+++ rewiew_1_1.h
@@ -0,0 +1,13 @@
+#ifndef REWIEW_1_1_H
+#define REWIEW_1_1_H
+
+#include <vector>
+
+// FIX_ME: Неправильное именование функции (должно быть lower_case_with_underscores)
+// старый код: void knapSack(int W, vector<int>& wt, vector<int>& val) {
+// новый код:
+void FindMinTaxSubset(int min_weight,
+                     const std::vector<int>& weights,
+                     const std::vector<int>& taxes);
+
+#endif // REWIEW_1_1_H
